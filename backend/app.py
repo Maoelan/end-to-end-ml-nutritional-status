75ff310a682ef576b1dcbe384e2f3eda5918582c
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2023-05-15 13:01:11 +0700

    venv & api endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,93 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from config.config import Config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/data_gizi'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import OrangTua, Anak, Gizi, User
+
+@app.route('/api/orangtua/get', methods=['GET'])
+def get_orangtua():
+    orangtua = OrangTua.query.all()
+    result = []
+
+    for x in orangtua:
+        data = {
+            'id' : x.id,
+            'nama' : x.nama,
+            'alamat' : x.alamat,
+            'provinsi' : x.provinsi,
+            'kabupaten' : x.kabupaten,
+            'kecamatan' : x.desa,
+            'posyandu' : x.posyandu,
+            'rt' : x.rt,
+            'rw' : x.rw 
+        }
+        result.append(data)
+    
+    return jsonify(result)
+
+@app.route('/api/orangtua/add', methods=['POST'])
+def add_orangtua():
+    data = request.get_json()
+
+    orangtua = OrangTua(
+        nama = data['nama'],
+        alamat = data['alamat'],
+        provinsi = data['provinsi'],
+        kabupaten = data['kabupaten'],
+        kecamatan = data['kecamatan'],
+        desa = data['desa'],
+        posyandu = data['posyandu'],
+        rt = data['rt'],
+        rw = data['rw']
+    )
+
+    db.session.add(orangtua)
+    db.session.commit()
+
+    return jsonify({'message': 'Data orang tua berhasil ditambahkan'})
+
+@app.route('/api/orangtua/update/<int:id>', methods=['PUT'])
+def update_orangtua(id):
+    data = request.get_json()
+
+    orangtua = OrangTua.query.get(id)
+    if orangtua is None:
+        return jsonify({'message': 'Data orang tua tidak ditemukan'})
+    
+    orangtua.nama = data['nama']
+    orangtua.alamat = data['alamat']
+    orangtua.provinsi = data['provinsi']
+    orangtua.kabupaten = data['kabupaten']
+    orangtua.kecamatan = data['kecamatan']
+    orangtua.desa = data['desa']
+    orangtua.posyandu = data['posyandu']
+    orangtua.rt = data['rt']
+    orangtua.rw = data['rw']
+
+    db.session.commit()
+
+    return jsonify({'message': 'Data orang tua berhasil diupdate'})
+
+@app.route('/api/orangtua/delete/<int:id>', methods=['DELETE'])
+def delete_orangtua(id):
+    orangtua = OrangTua.query.get(id)
+    if orangtua is None:
+        return jsonify({'message': 'Data orang tua tidak ditemukan'})
+    
+    db.session.delete(orangtua)
+    db.session.commit()
+
+    return jsonify({'message': 'Data orang tua berhasil dihapus'})
+
+
 
 if __name__ == '__main__':
     app.run()
+
+
--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class orangtua(db.Model):
+class OrangTua(db.Model):
     __tablename__ = 'data_orang_tua'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,37 +13,45 @@ class orangtua(db.Model):
     posyandu = db.Column(db.String(100))
     rt = db.Column(db.String(10))
     rw = db.Column(db.String(10))
+    anak = db.relationship('Anak', backref='orang_tua', lazy=True)
 
-class anak(db.Model):
+class Anak(db.Model):
     __tablename__ = 'data_anak'
 
     id = db.Column(db.Integer, primary_key=True)
     nik = db.Column(db.String(16))
-    nama  = db.Column(db.String(255))
+    nama = db.Column(db.String(255))
     jenis_kelamin = db.Column(db.String(10))
-    tanggal_lahir = db.Column(db.Data)
-    berat_lahir = db.Column(db.Flaot(5,2))
-    tinggi_lahir = db.Column(db.Float(5,2))
-    id_orangtua = db.Column(db.Integer, db.ForeignKey('data_orang_tua.id'))
-    
-    orangtua = db.relationship('orangtua', backref=db.backref('anak', lazy=True))
+    tanggal_lahir = db.Column(db.Date)
+    berat_lahir = db.Column(db.Float(5, 2))
+    tinggi_lahir = db.Column(db.Float(5, 2))
+    id_orang_tua = db.Column(db.Integer, db.ForeignKey('data_orang_tua.id'))
+    data_gizi = db.relationship('Gizi', backref='anak', lazy=True)
 
-class gizi(db.model):
+class Gizi(db.Model):
     __tablename__ = 'data_gizi'
     
     id = db.Column(db.Integer, primary_key=True)
-    id_anak = db.column(db.Integer, db.ForeignKey('data_anak.id'))
+    id_anak = db.Column(db.Integer, db.ForeignKey('data_anak.id'))
     usia_diukur = db.Column(db.Integer)
-    tanggal_pengukuran = db.Column(db.Data)
-    berat = db.Column(db.Float(5,2))
-    tinggi = db.Column(db.Float(5,2))
-    lingkar_lengan_atas = db.Column(db.Float(5,2))
-    zs_bb_umur = db.Column(db.Float(5,2))
-    zz_bb_tb = db.Column(db.Float(5,2))
-    bb_umur = db.Column(db.Float(5,2))
-    bb_tb = db.Column(db.Float(5,2))
+    tanggal_pengukuran = db.Column(db.Date)
+    berat = db.Column(db.Float(5, 2))
+    tinggi = db.Column(db.Float(5, 2))
+    lingkar_lengan_atas = db.Column(db.Float(5, 2))
+    z_score_berat_badan_umur = db.Column(db.Float(5, 2))
+    z_score_berat_badan_tinggi_badan = db.Column(db.Float(5, 2))
+    berat_badan_umur = db.Column(db.Float(5, 2))
+    berat_badan_tinggi_badan = db.Column(db.Float(5, 2))
     naik_berat_badan = db.Column(db.String(10))
-    jumlah_vitamin_a = (db.Integer)
-    
+    jumlah_vitamin_a = db.Column(db.Integer)
+
+class User(db.Model):
+    __tablename__ = 'data_login'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    role = db.Column(db.String(50))
         
 
