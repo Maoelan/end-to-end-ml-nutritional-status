3e5dd5e3abc4ab820f6cbb110b10f53be8f96815
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2023-05-25 00:21:07 +0700

    refresh orangtua-read

--- frontend/src/components/crud/orangtua/orangtua-read.jsx
+++ frontend/src/components/crud/orangtua/orangtua-read.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import MainLayout from '../../design/MainLayout';
 import '../../css/orangtua.css'
+import { Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const OrangTuaRead = () => {
   const [orangtuaList, setOrangTuaList] = useState([]);
@@ -44,38 +46,38 @@ const OrangTuaRead = () => {
       <div className="orangtua-read-container">
         <h2>Daftar Orang Tua</h2>
         <div className="table-container">
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nama</th>
-                <th>Alamat</th>
-                <th>Provinsi</th>
-                <th>Kabupaten</th>
-                <th>Kecamatan</th>
-                <th>Desa</th>
-                <th>Posyandu</th>
-                <th>RT</th>
-                <th>RW</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orangtuaList.map((orangtua) => (
-                <tr key={orangtua.id}>
-                  <td>{orangtua.id}</td>
-                  <td>{orangtua.nama}</td>
-                  <td>{orangtua.alamat}</td>
-                  <td>{orangtua.provinsi}</td>
-                  <td>{orangtua.kabupaten}</td>
-                  <td>{orangtua.kecamatan}</td>
-                  <td>{orangtua.desa}</td>
-                  <td>{orangtua.posyandu}</td>
-                  <td>{orangtua.rt}</td>
-                  <td>{orangtua.rw}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Provinsi</th>
+            <th>Kabupaten</th>
+            <th>Kecamatan</th>
+            <th>Desa</th>
+            <th>Posyandu</th>
+            <th>RT</th>
+            <th>RW</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orangtuaList.map((orangtua) => (
+            <tr key={orangtua.id}>
+              <td>{orangtua.id}</td>
+              <td>{orangtua.nama}</td>
+              <td>{orangtua.alamat}</td>
+              <td>{orangtua.provinsi}</td>
+              <td>{orangtua.kabupaten}</td>
+              <td>{orangtua.kecamatan}</td>
+              <td>{orangtua.desa}</td>
+              <td>{orangtua.posyandu}</td>
+              <td>{orangtua.rt}</td>
+              <td>{orangtua.rw}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
         </div>
       </div>
     </MainLayout>
