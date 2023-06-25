f6197b14c48e983222c450432329f015b29db883
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2023-06-26 00:00:12 +0700

    train k-means & frontend

--- backend/train_kmeans.py
+++ backend/train_kmeans.py
@@ -29,22 +29,24 @@ def train_kmeans():
 
     total_iterations = kmeans.n_iter_
 
-    previous_centroids = None
     for i in range(total_iterations):
         iteration = {}
-        kmeans.fit(scaled_data)
+        current_kmeans = KMeans(n_clusters=n_clusters, init='random')
+        current_kmeans.cluster_centers_ = kmeans.cluster_centers_
+        current_kmeans.labels_ = kmeans.predict(scaled_data)
 
         for j in range(n_clusters):
-            cluster_points = scaled_data[kmeans.labels_ == j].tolist()
+            cluster_points = []
+            for index, point in enumerate(scaled_data):
+                if current_kmeans.labels_[index] == j:
+                    cluster_points.append(point.tolist())
             iteration[f'cluster_{j+1}'] = cluster_points
 
-        if previous_centroids is not None:
-            centroid_changes = kmeans.cluster_centers_ - previous_centroids
-            iteration['centroid_changes'] = scaler.inverse_transform(centroid_changes).tolist()
-
-        previous_centroids = kmeans.cluster_centers_
         iterations.append(iteration)
 
+        # Update centroid
+        kmeans.fit(scaled_data)
+    
     cluster_count = n_clusters
     centroid_changes = kmeans.inertia_
 
