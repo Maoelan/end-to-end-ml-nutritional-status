a42365db06e833eebadb395b9c04dfd04974170d
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2023-07-10 10:42:13 +0700

    finals

--- backend/train_kmedoids.py
+++ backend/train_kmedoids.py
@@ -1,6 +1,6 @@
-from sklearn_extra.cluster import KMedoids
-from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 from scipy.spatial.distance import cdist
+from sklearn_extra.cluster import KMedoids
 from flask import jsonify
 from models import Anak, Gizi
 
@@ -17,35 +17,37 @@ def train_kmedoids():
             g.tinggi
         ])
 
-    scaler = MinMaxScaler()
-    scaled_data = scaler.fit_transform(data)
+    scaled_data = np.array(data)
 
     n_clusters = 3
     random_state = 12
 
-    kmedoids = KMedoids(n_clusters=n_clusters, init='random', random_state=random_state, metric='euclidean')
+    initial_medoids = np.array([
+        [3.1, 51, 11, 73.3],  
+        [2.9, 50, 11.6, 88],  
+        [2.7, 51, 10.5, 87.7]
+    ])
+
+    kmedoids = KMedoids(n_clusters=n_clusters, init=initial_medoids, max_iter=4, random_state=random_state, metric='euclidean')
     kmedoids.fit(scaled_data)
 
     iterations = []
-    initial_medoids = scaler.inverse_transform(kmedoids.cluster_centers_)  
-    initial_medoids = scaler.fit_transform(initial_medoids).tolist()  
-
     total_iterations = kmedoids.n_iter_
 
     for i in range(total_iterations):
         iteration = {}
-        current_kmedoids = KMedoids(n_clusters=n_clusters, init='random', random_state=random_state, metric='euclidean')
+        current_kmedoids = KMedoids(n_clusters=n_clusters, init=kmedoids.medoid_indices_, random_state=random_state, metric='euclidean')
         current_kmedoids.medoid_indices_ = kmedoids.medoid_indices_
         current_kmedoids.labels_ = kmedoids.predict(scaled_data)
 
         medoid_changes = []
+
         for j in range(n_clusters):
-            cluster_indices = [index for index, label in enumerate(current_kmedoids.labels_) if label == j]
+            cluster_indices = np.where(current_kmedoids.labels_ == j)[0]
             cluster_points = scaled_data[cluster_indices]
-            distances = cdist(cluster_points, cluster_points, metric='cityblock')
-            new_medoid_index = distances.sum(axis=1).argmin()
-            medoid_change = abs(cluster_points[new_medoid_index] - scaled_data[current_kmedoids.medoid_indices_[j]])
-            medoid_changes.append(medoid_change.tolist())
+            medoid_index = kmedoids.medoid_indices_[j]
+            medoid_change = np.mean(cdist(cluster_points, np.expand_dims(scaled_data[medoid_index], axis=0)))
+            medoid_changes.append(medoid_change)
 
         iteration['medoid_changes'] = medoid_changes
 
@@ -59,29 +61,28 @@ def train_kmedoids():
         iterations.append(iteration)
 
         kmedoids.fit(scaled_data)
+
+        print(f"Iteration {i+1} - Medoid Changes:")
+        for j, medoid_change in enumerate(medoid_changes):
+            print(f"M{j+1}: {medoid_change}")
     
     cluster_count = n_clusters
-    medoid_changes = kmedoids.inertia_
-
     cluster_labels = []
     for label in kmedoids.labels_:
         if label == 2:
-            cluster_labels.append("GIZI LEBIH")
+            cluster_labels.append("GIZI KURANG")
         elif label == 1:
             cluster_labels.append("GIZI BAIK")
         else:
-            cluster_labels.append("GIZI KURANG")
-
-    scaled_data = scaled_data.tolist()
+            cluster_labels.append("GIZI LEBIH")
 
     response = {
         'iterations': iterations,
         'cluster_count': cluster_count,
-        'medoid_changes': medoid_changes,
         'cluster_labels': cluster_labels,
-        'initial_medoids': initial_medoids,
+        'initial_medoids': initial_medoids.tolist(),
         'total_iterations': total_iterations,
-        'scaled_data': scaled_data
+        'scaled_data': scaled_data.tolist()
     }
 
     return response
